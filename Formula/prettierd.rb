2e128f1811c514823e3f59f5b210a70f5c60e598
fsouzabot <fsouzabot@users.noreply.github.com>
2023-07-12 22:49:30 +0000

    prettierd v0.24.2

--- Formula/prettierd.rb
+++ Formula/prettierd.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Prettierd < Formula
   desc "prettier, on SPEED!"
   homepage "https://github.com/fsouza/prettierd"
-  url "https://registry.npmjs.org/@fsouza/prettierd/-/prettierd-0.24.1.tgz"
-  sha256 "5b3896c5b3a72a97c2bc28240f46940b52287b0c48487c841e30725a75b8e82c"
+  url "https://registry.npmjs.org/@fsouza/prettierd/-/prettierd-0.24.2.tgz"
+  sha256 "1dfe44ebfad78799fcaf477cd9fdb74be4095b1646433de2b13f490498106670"
   license "ISC"
   head "https://github.com/fsouza/prettierd.git", branch: "main"
 
